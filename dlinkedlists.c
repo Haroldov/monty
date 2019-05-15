4e36fc37c1cacc6f884dae9c83f06efc73cc7fca
Haroldo Vélez <hvelez020@gmail.com>
2019-05-15 16:11:18 +0000

    [add] dlinkedlist toolbox

---
+++ dlinkedlists.c
@@ -0,0 +1,21 @@
+/**
+ *print_dlistint - prints all the elements of a dlistint_t list.
+ *@h: header of the doubly linked list
+ *Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int numNodes = 0;
+
+	if (h == NULL)
+		return (numNodes);
+	while (h != NULL)
+	{
+		printf("%i\n", (*h).n);
+		h = (*h).next;
+		numNodes++;
+	}
+	return (numNodes);
+}
+
