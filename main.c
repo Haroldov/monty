452b7fdcfb12b132635ec66ab4704e95be4a0fa8
Haroldo Vélez <hvelez020@gmail.com>
2019-05-16 04:12:32 +0000

    [fix] betty style

--- main.c
+++ main.c
@@ -22,16 +22,12 @@ int main(int argc, char *argv[])
 	void (*cmd)(stack_t **stack, unsigned int line_number) = NULL;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+	{       fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);    }
 	stream = fopen(argv[1], "r");
 	if (stream == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+	{       fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);    }
 	while (getline(&line, &line_size, stream) != -1)
 	{
 		line_num++;
