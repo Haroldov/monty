65d82d36542cca30e9a8e0ede0bae402959527c8
Haroldo <hvelez020@gmail.com>
2019-05-16 17:48:02 +0000

    [betty] style

--- monty_ops#2.c
+++ monty_ops#2.c
@@ -60,22 +60,22 @@ void op_mod(stack_t **dlinkedlist, unsigned int line_num)
 	if (dlistint_len(*dlinkedlist) < 2)
 	{
 		fprintf(stderr, "L%i: can't mod, stack too short\n", line_num);
-    free_dlistint(*dlinkedlist), free(carrier.words), free(carrier.line);
-    fclose(carrier.stream);
-    exit(EXIT_FAILURE)
-  }  
-    if ((*dlinkedlist)->n == 0)
-	  { 
-		  fprintf(stderr, "L%i: division by zero\n", line_num);
-		  free_dlistint(*dlinkedlist), free(carrier.words), free(carrier.line);
-		  fclose(carrier.stream);
-		  exit(EXIT_FAILURE);
-	  }
-	  a = (*dlinkedlist)->n;
-	  b = (*dlinkedlist)->next->n;
-	  b = b % a;
-	  delete_head(dlinkedlist);
-	  (*dlinkedlist)->n = b;
+		free_dlistint(*dlinkedlist), free(carrier.words), free(carrier.line);
+		fclose(carrier.stream);
+		exit(EXIT_FAILURE);
+	}
+	if ((*dlinkedlist)->n == 0)
+	{
+		fprintf(stderr, "L%i: division by zero\n", line_num);
+		free_dlistint(*dlinkedlist), free(carrier.words), free(carrier.line);
+		fclose(carrier.stream);
+		exit(EXIT_FAILURE);
+	}
+	a = (*dlinkedlist)->n;
+	b = (*dlinkedlist)->next->n;
+	b = b % a;
+	delete_head(dlinkedlist);
+	(*dlinkedlist)->n = b;
 }
 
 /**
