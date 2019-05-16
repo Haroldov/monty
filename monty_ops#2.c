327b1dfdfcdd897d2b90cbcf84607162fa2eea79
Haroldo <hvelez020@gmail.com>
2019-05-16 16:35:22 +0000

    [add] task #5

---
+++ monty_ops#2.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+
+/**
+ *op_nop - doesn’t do anything.
+ *@dlinkedlist: the header of the stack or queue
+ *@line_num: this is unused in this function
+ *Return: none
+ */
+
+void op_nop(stack_t **dlinkedlist, unsigned int line_num)
+{
+	(void) line_num;
+	(void) dlinkedlist;
+}
